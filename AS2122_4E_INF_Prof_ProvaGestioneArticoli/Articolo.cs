fec54d9a118cd34c46703d831f305dcebd254290
Billone23 <bryan.decarlo@studenti.ittsrimini.edu.it>
2022-03-16 10:04:36 +0100

    // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -15,7 +15,7 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
         private double prezzo;
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
-        // ...
+        // 
         public string Descrizione{ set; get; }
         public string UnitaMisura { set; get; }
         public double Prezzo { set; get; }
